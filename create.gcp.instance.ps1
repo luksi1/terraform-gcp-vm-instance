b3c84817101ec98d70c7bab625d4f32c3ee7905e
luksi1 <luke@springboot.europe-west4-a.c.playground-250603.internal>
2020-02-19 18:46:48 +0000

    Added pwsh and openjdk 11

--- create.gcp.instance.ps1
+++ create.gcp.instance.ps1
@@ -3,7 +3,25 @@ param (
   [string] $instance_name,
   [parameter(Mandatory)]
   [string] $project,
-  [string] $credentials = 'secrets/secret.json'
+  [string] $region = 'europe-west4',
+  [string] $zone = 'europe-west4-a',
+  [string] $image = 'ubuntu-1804-lts',
+  [string] $machine_type = 'f1-micro',
+  [string] $ssh_pub_key_file = './id_rsa.pub',
+  [string] $credentials_file = 'secrets/secret.json'
 )
 
-Invoke-Expression "& terraform apply -auto-approve -var=`"project=$project`" -var=`"instance_name=$instance_name`" -var=`"credentials=$credentials`""
+[hashtable] $vars = @{
+  instance_name    = $instance_name;
+  project          = $project;
+  region           = $region;
+  zone             = $zone;
+  image            = $image;
+  machine_type     = $machine_type;
+  ssh_pub_key_file = $ssh_pub_key_file;
+  credentials_file = $credentials_file
+}
+
+$vars.Keys | ForEach-Object { $variables += "-var=`"$_=$vars.Item($_)`" " }
+
+Invoke-Expression "& terraform apply -auto-approve $variables"
